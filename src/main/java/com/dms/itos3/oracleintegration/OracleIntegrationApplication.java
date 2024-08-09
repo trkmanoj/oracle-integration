837ce0bf1dd7403d3ed4f0e2b467e94612420fcf
trkmanoj <53530055+trkmanoj@users.noreply.github.com>
2024-08-09 15:36:02 +0530

    Add files via upload

---
+++ src/main/java/com/dms/itos3/oracleintegration/OracleIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dms.itos3.oracleintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dms/itos3/oracleintegration/OracleIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.itos3.oracleintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
